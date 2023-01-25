ce4e969e3ee51e69c4e3ebf2658ccc2f5c73ccf4
MJ Sung <themrsung@gmail.com>
2023-01-26 01:39:28 +0900

    provider

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import AddItem from "./components/portfolio/AddItem"
 import NavigationBar from "./components/NavigationBar"
 import PortfolioItemDetails from "./pages/portfolio/PortfolioItemDetails"
 import MyPortfolio from "./pages/portfolio/MyPortfolio"
+import { store } from "./redux/config"
+import { Provider } from "react-redux"
 
 function App() {
     return (
-        <BrowserRouter>
-            <NavigationBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
-                <Route path="additem" element={<AddItem />} />
-                <Route path="item" element={<PortfolioItemDetails />} />
-                <Route path="portfolio" element={<MyPortfolio />} />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <NavigationBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="additem" element={<AddItem />} />
+                    <Route path="item" element={<PortfolioItemDetails />} />
+                    <Route path="portfolio" element={<MyPortfolio />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     )
 }
 
