2d5e235690a48267392c5afc39f106925e57b7d8
MJ Sung <themrsung@gmail.com>
2023-01-26 06:20:32 +0900

    passphrase edit

--- src/pages/portfolio/MyPortfolio.jsx
+++ src/pages/portfolio/MyPortfolio.jsx
@@ -3,9 +3,12 @@ import { getCurrentlyLoggedInUserId, isLoggedIn } from "../../api/authApi"
 import { store } from "../../redux/config"
 import { getDecryptedItemsOfUser } from "../../api/itemsApi"
 import PortfolioItemList from "../../components/portfolio/PortfolioItemList"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { setPassphrase } from "../../redux/reducers/passphraseReducer"
 
 export default function MyPortfolio() {
+    const dispatch = useDispatch()
+
     const [items, setItems] = useState([])
 
     const passphrase = useSelector((state) => state.passphrase.passphrase)
@@ -30,6 +33,15 @@ export default function MyPortfolio() {
 
     return (
         <>
+            <div>
+                <input
+                    value={passphrase}
+                    type="password"
+                    onChange={(e) => {
+                        dispatch(setPassphrase(e.target.value))
+                    }}
+                />
+            </div>
             <PortfolioItemList decryptedItems={items} />
         </>
     )
