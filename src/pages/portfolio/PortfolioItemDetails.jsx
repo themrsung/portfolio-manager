e53c5c3f05f24dde4aaddd038c4647773fd5b651
MJ Sung <themrsung@gmail.com>
2023-01-26 02:18:51 +0900

    d

--- src/pages/portfolio/PortfolioItemDetails.jsx
+++ src/pages/portfolio/PortfolioItemDetails.jsx
@@ -19,6 +19,8 @@ export default function PortfolioItemDetails() {
         fetchItem()
     }, [])
 
+    console.log(decryptedItem)
+
     return <PortfolioItem decryptedItem={decryptedItem} />
     // return <PortfolioItem decryptedItem={decryptedItem} />
 }
