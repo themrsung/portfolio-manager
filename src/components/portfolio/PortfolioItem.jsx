bfedc331fc5fe6089eeb0fd63d71c04f94c671e7
MJ Sung <themrsung@gmail.com>
2023-01-26 01:05:04 +0900

    test

--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,27 +1,10 @@
 import { useEffect, useState } from "react"
 
 export default function PortfolioItem({ decryptedItem }) {
-    const [itemProps, setItemProps] = useState([])
-
-    useEffect(() => {
-        for (const key in decryptedItem) {
-            if (decryptedItem.hasOwnProperty(key)) {
-                setItemProps([
-                    ...itemProps,
-                    { key: key, value: decryptedItem[key] }
-                ])
-            }
-        }
-    }, [decryptedItem])
-
     return (
         <>
-            {itemProps.map((ip) => {
-                return (
-                    <p key={ip.key}>
-                        {ip.key} / {ip.value}
-                    </p>
-                )
+            {decryptedItem.map((prop) => {
+                console.log(prop)
             })}
         </>
     )
